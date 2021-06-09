034424b808dbff43805d7426c3dc2ce5a46a4593
Davide Manna <davide.manna@strath.ac.uk>
2021-06-09 17:37:49 +0100

    Modify run_powershell.ps1 by adding parameters

--- run_powershell.ps1
+++ run_powershell.ps1
@@ -1,1 +1,21 @@
-docker run --rm --name Photomosaic --mount type=bind, source="C:\Users\ixb20175\Desktop\Projects\Docker\photomosaic", target="/project" --mount type=bind, source="C:\Users\ixb20175\Pictures", target="/pics" -p "8888:8888/tcp" -e "DISPLAY=
+param($PicturesDir="c:\Users\$env:UserName\Pictures", $ProjectDir="$pwd", $ContainerName="Photomosaic")
+
+$env:HostIP = (
+    Get-NetIPConfiguration |
+    Where-Object {
+        $_.IPv4DefaultGateway -ne $null -and
+        $_.NetAdapter.Status -ne "Disconnected"
+    }
+).IPv4Address.IPAddress
+
+docker run --rm --name $ContainerName `
+	--mount type=bind,source="$ProjectDir",target="/project" `
+	--mount type=bind,source="$PicturesDir",target="/pics" `
+	-p "8888:8888/tcp" -e "DISPLAY=$env:HostIP:0" `
+	mosaic-conda:latest `
+	jupyter notebook `
+		--allow-root `
+		--ip 0.0.0.0 `
+		--no-browser `
+		--NotebookApp.token='' `
+		--notebook-dir=/project
